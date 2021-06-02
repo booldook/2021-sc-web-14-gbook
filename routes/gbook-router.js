b1141c178d89ef502ae4b98bf6888e24281f029c
booldook <booldook@gmail.com>
2021-06-02 17:56:33 +0900

    gbook create 완료!

--- routes/gbook-router.js
+++ routes/gbook-router.js
@@ -8,8 +8,6 @@ const ejs = {
 	tabTitle: 'Express 방명록',
 	pageTitle: 'Express를 활용한 방명록 서비스',
 	pageDesc: 'express, ejs, multer, mysql 등을 사용한 방명록',
-	css: 'gbook',
-	js: 'gbook'
 }
 
 router.get('/', async (req, res, next) => {
