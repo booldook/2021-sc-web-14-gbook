719ba5272a88e37fd1bfcb9dac0b2dc183291f8b
booldook <booldook@gmail.com>
2021-06-02 17:54:04 +0900

    gbook create 완료

--- routes/gbook-router.js
+++ routes/gbook-router.js
@@ -18,14 +18,23 @@ router.get('/', async (req, res, next) => {
 
 router.post('/create', upload.single('upfile'), async (req, res, next) => {
 	try {
+		let sql, values; 
+
+		// gbook 저장
 		let { writer, content } = req.body;
+		sql = 'INSERT INTO gbook SET writer=?, content=?';
+		values = [writer, content];
+		const [r] = await pool.execute(sql, values);
+
 		if(req.file) {
+			//gbookfile 저장
 			let { originalname, filename, size, mimetype } = req.file;
-			res.json({ writer, content, originalname, filename, size, mimetype });
+			sql = 'INSERT INTO gbookfile SET oriname=?, savename=?, size=?, type=?, gid=?';
+			values = [originalname, filename, size, mimetype, r.insertId];
+			const [r2] = await pool.execute(sql, values);
+			res.json(r2);
 		}
-		else {
-			res.json({ writer, content });
-		}
+
 	}
 	catch(err) {
 		next(error(err));
