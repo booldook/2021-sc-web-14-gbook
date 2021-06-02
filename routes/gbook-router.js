57878964fe1303bc905aaf01eb3bba1ebc80a2ce
booldook <booldook@gmail.com>
2021-06-02 15:48:09 +0900

    UI구현중

--- routes/gbook-router.js
+++ routes/gbook-router.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const ejs = {
+	tabTitle: 'Express 방명록',
+	pageTitle: 'Express를 활용한 방명록 서비스',
+	pageDesc: 'express, ejs, multer, mysql 등을 사용한 방명록',
+	css: 'gbook',
+	js: 'gbook'
+}
+
 router.get('/', async (req, res, next) => {
-	res.send('<h1>Hello Gbook</h1>');
+	res.render('gbook/gbook', { ...ejs });
 });
 
 module.exports = router;
