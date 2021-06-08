ab6f12f641eda76a7b043235a536f04b9fa4b44b
booldook <booldook@gmail.com>
2021-06-08 16:37:54 +0900

    sql upload

--- SQL/gbook.sql
+++ SQL/gbook.sql
@@ -1,76 +1,3 @@
--- --------------------------------------------------------
--- 호스트:                          localhost
--- 서버 버전:                        8.0.25 - MySQL Community Server - GPL
--- 서버 OS:                        Win64
--- HeidiSQL 버전:                  11.2.0.6213
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
--- 테이블 shop.gbook 구조 내보내기
-CREATE TABLE IF NOT EXISTS `gbook` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `content` varchar(255) NOT NULL,
-  `writer` varchar(255) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `uid` int unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `uid` (`uid`),
-  CONSTRAINT `FK_gbook_users` FOREIGN KEY (`uid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
--- 테이블 shop.gbookfile 구조 내보내기
-CREATE TABLE IF NOT EXISTS `gbookfile` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `gid` int unsigned NOT NULL,
-  `oriname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `savename` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `size` int unsigned NOT NULL,
-  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `pid` (`gid`) USING BTREE,
-  CONSTRAINT `FK_gbookfile_gbook` FOREIGN KEY (`gid`) REFERENCES `gbook` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
-
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
--- 테이블 shop.product 구조 내보내기
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `prdname` varchar(255) NOT NULL,
-  `price` int unsigned NOT NULL DEFAULT '0',
-  `content` text,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
--- 테이블 shop.productfile 구조 내보내기
-CREATE TABLE IF NOT EXISTS `productfile` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `pid` int unsigned NOT NULL,
-  `oriname` varchar(255) NOT NULL,
-  `savename` varchar(255) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `size` int unsigned NOT NULL,
-  `type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `pid` (`pid`),
-  CONSTRAINT `FK_productfile_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
--- 테이블 shop.users 구조 내보내기
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `userid` varchar(50) NOT NULL,
@@ -81,11 +8,32 @@ CREATE TABLE IF NOT EXISTS `users` (
   `expiredAt` datetime DEFAULT NULL,
   `grade` tinyint unsigned NOT NULL DEFAULT '2' COMMENT '0:탈퇴회원 1:유휴회원 2:회원 3:우수 9:관리자',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+CREATE TABLE IF NOT EXISTS `gbook` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) NOT NULL,
+  `writer` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `uid` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `FK_gbook_users` FOREIGN KEY (`uid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `gbookfile` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `gid` int unsigned NOT NULL,
+  `oriname` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `savename` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `size` int unsigned NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `pid` (`gid`) USING BTREE,
+  CONSTRAINT `FK_gbookfile_gbook` FOREIGN KEY (`gid`) REFERENCES `gbook` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4  ROW_FORMAT=DYNAMIC;
+
+
